545a08c29b6fb0240cac6f6351ffb2cf5fd3b45a
JessyLopM06 <jessylopman25@outlook.com>
2024-05-06 12:37:17 -0600

    roles

---
+++ src/app/helpers/api/v1/roles/index.ts
@@ -0,0 +1,20 @@
+import { UserOption } from "@/app/molecule/userAccess/types";
+import { isOkRes } from "../../../fetch";
+
+export async function fetchRoles(): Promise<UserOption[]> {
+try {
+    const res = await fetch("/api/v1/roles", {
+    method: "GET",
+    });
+
+    if (isOkRes(res)) {
+    const users = await res.json();
+    return users;
+    }
+
+    return []
+} catch (error) {
+    console.log(error)
+    return [];
+}
+}
