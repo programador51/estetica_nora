857a2dc4dfc6c5ac6738d9ac1f459f7f0f622899
Jose Luis <gskitcat@gmail.com>
2024-05-13 20:24:17 -0600

    citas
    contexto valor inicial

--- src/app/Contexts/ServiceContext/index.ts
+++ src/app/Contexts/ServiceContext/index.ts
@@ -3,12 +3,24 @@ import { createContext } from "react";
 
 const ContextService = createContext<ReturnUseService>({
   isLoading: true,
-  appendService:()=>{},
-  deleteService:()=>{},
-  durationOnMinutes:0,
-  promptCancelation:async()=>{},
-  services:[],
-  total:0
+  appendService: () => {},
+  deleteService: () => {},
+  durationOnMinutes: 0,
+  promptCancelation: async () => {},
+  services: [],
+  total: 0,
+  day: new Date(),
+  disabledWeekDays: [],
+  maxTime: 0,
+  minTime: 0,
+  schedule: {
+    isLoading: false,
+    schedules: [],
+  },
+  setDayReservation: () => {},
+  setTimeReservation: () => {},
+  tileDisabled: () => false,
+  timeReservation: 0,
 });
 
 export default ContextService;
