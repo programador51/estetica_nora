64d84d920d1b7f5d981eff80d4731e9d71fdd1b8
Jose Luis <gskitcat@gmail.com>
2024-05-15 22:33:18 -0600

    reservaciones
    reservaciones consultadas desde api y mostradas en ui

--- src/app/customHooks/useReservations/index.ts
+++ src/app/customHooks/useReservations/index.ts
@@ -1,37 +1,41 @@
 import { useEffect, useState } from "react";
 import { ReturnUseReservationItem, StateUseReservations } from "./types";
+import { getReservationsPaginated } from "@/app/helpers/api/v1/reservation";
+import { ReservationItem } from "@/app/molecule/reservationItem/types";
 
 const INITIAL_STATE: StateUseReservations = {
   page: 1,
   isLoading: true,
-  reservations: [
-    {
-      urlPicture: "https://www.mnp.ca/-/media/foundation/integrations/personnel/2020/12/16/13/57/personnel-image-4483.jpg?h=800&w=600&hash=9D5E5FCBEE00EB562DCD8AC8FDA8433D",
-      reservation: new Date("2024-04-15T08:00:00"), // Example date
-      name: "José Luis",
-    },
-    {
-      urlPicture: "https://www.mnp.ca/-/media/foundation/integrations/personnel/2020/12/16/13/51/personnel-image-3287.jpg?h=800&w=600&hash=F23BF4998F05D0DC079DE43DD0E58507",
-      reservation: new Date("2024-04-16T10:00:00"), // Example date
-      name: "Sandra",
-    },
-    {
-      urlPicture: null,
-      reservation: new Date("2024-04-17T12:00:00"), // Example date
-      name: "Erick",
-    },
-  ],
+  reservations: [],
 };
 
 export default function useReservations(): ReturnUseReservationItem {
   const [state, setState] = useState(INITIAL_STATE);
 
   useEffect(() => {
-    setState((current) => ({
-      ...current,
-      isLoading: false,
-    }));
-  }, []);
+    (async function () {
+      setState((current) => ({
+        ...current,
+        isLoading: true,
+      }));
+
+      const reservations = await getReservationsPaginated(state.page);
+
+      const itemsParsed: ReservationItem[] = reservations.records.map(
+        (item) => ({
+          name: item.nombre,
+          reservation: new Date(item.fechaReservacion),
+          urlPicture: null,
+        })
+      );
+
+      setState((current) => ({
+        ...current,
+        isLoading: false,
+        reservations: itemsParsed,
+      }));
+    })();
+  }, [state.page]);
 
   return {
     ...state,
--- src/app/helpers/api/v1/reservation/index.ts
+++ src/app/helpers/api/v1/reservation/index.ts
@@ -2,6 +2,8 @@ import { DtoAddReservation } from "@/app/api/v1/reservation/types";
 import { promptError } from "@/app/helpers/alerts";
 import { CustomError } from "@/app/helpers/errors/types";
 import { isOkRes } from "@/app/helpers/fetch";
+import { DtoReservationPaginated } from "@/app/models/reservations/types";
+import { ResDtoPaginated } from "../types";
 
 export async function addReservation(dto: DtoAddReservation) {
   try {
@@ -22,5 +24,32 @@ export async function addReservation(dto: DtoAddReservation) {
   } catch (error) {
     promptError(error as CustomError);
     return false;
+  }
+}
+
+export async function getReservationsPaginated(
+  page: number
+): Promise<ResDtoPaginated<DtoReservationPaginated>> {
+  const error_data: ResDtoPaginated<DtoReservationPaginated> = {
+    page: 1,
+    pages: 0,
+    records: [],
+    noRecordsFound: 0,
+  };
+
+  try {
+    const res = await fetch(`/api/v1/reservation/page/${page}`, {
+      method: "GET",
+    });
+
+    if (isOkRes(res)) {
+      const data: ResDtoPaginated<DtoReservationPaginated> = await res.json();
+
+      return data;
+    }
+
+    return error_data;
+  } catch (error) {
+    return error_data;
   }
 }
