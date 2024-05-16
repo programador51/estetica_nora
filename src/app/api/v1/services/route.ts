1acd777687d0c635cb2cac10134dc48ff2a7f9cb
Jose Luis <gskitcat@gmail.com>
2024-05-15 20:02:54 -0600

    servicios
    api obtiene servicios desde la bd, ya no son en duro

--- src/app/api/v1/services/route.ts
+++ src/app/api/v1/services/route.ts
@@ -8,68 +8,21 @@ import { retrieveFilesFromReq } from "@/app/helpers/api/v1/files";
 import { uploadFiles } from "@/app/models/gallery";
 import model from "@/app/models/services/index";
 import { model as modelGallery } from "@/app/models/gallery/index";
+import service from "@/app/models/services";
 
 export async function GET(req: NextRequest) {
-  const dbRecords: ServiceOption[] = [
-    {
-      name: "Haircut",
-      id: 1,
-      picture: "https://example.com/haircut.jpg",
-      costPrice: 20,
-      sellPrice: 30,
-      durationOnMinutes: 30,
-      toleranceOnMinutes: 5,
-      susceptibleToChange: true,
-    },
-    {
-      name: "Massage",
-      id: 2,
-      picture: "https://example.com/massage.jpg",
-      costPrice: 40,
-      sellPrice: 60,
-      durationOnMinutes: 60,
-      toleranceOnMinutes: 10,
-      susceptibleToChange: true,
-    },
-    {
-      name: "Manicure",
-      id: 3,
-      picture: "https://example.com/manicure.jpg",
-      costPrice: 15,
-      sellPrice: 25,
-      durationOnMinutes: 45,
-      toleranceOnMinutes: 5,
-      susceptibleToChange: true,
-    },
-    {
-      name: "Pedicure",
-      id: 4,
-      picture: "https://example.com/pedicure.jpg",
-      costPrice: 20,
-      sellPrice: 35,
-      durationOnMinutes: 60,
-      toleranceOnMinutes: 5,
-      susceptibleToChange: true,
-    },
-    {
-      name: "Facial",
-      id: 5,
-      picture: "https://example.com/facial.jpg",
-      costPrice: 50,
-      sellPrice: 80,
-      durationOnMinutes: 60,
-      toleranceOnMinutes: 10,
-      susceptibleToChange: true,
-    },
-  ];
-
   try {
-    return NextResponse.json(dbRecords, {
+    const services = await service.getAll();
+
+    const arrayServices = Object.entries(services).map(([key, value]) => value);
+
+    return NextResponse.json(arrayServices, {
       status: 200,
     });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json([]);
+    return NextResponse.json([], {
+      status: 500,
+    });
   }
 }
 
